22fb1999cfcac28e5344c9bdb4ee87c81e2bcb69
Seol'Lab <83863024+SeolLab@users.noreply.github.com>
2023-04-28 13:48:47 +0900

    Create 15649.cpp

---
+++ Backtracking/15649.cpp
@@ -0,0 +1,30 @@
+//15649 N과 M(1) c++
+
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[8];
+bool isused[8];
+
+void refunc(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+	cout << '\n';
+	return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (not isused[i]) {
+			arr[k] = i;
+			isused[i] = 1;
+			refunc(k+1);
+			isused[i] = 0;
+		}
+	}
+}
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	refunc(0);
+}
